0172284e3741c623032d98d88a4041aea576b61e
Bogdan Pshonyak <bpshonyak@live.com>
2016-07-10 18:39:29 +0900

    some custom colors

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {cyan500} from 'material-ui/styles/colors';
+
+const muiTheme = getMuiTheme({
+    palette: {
+      primary1Color: '#FFD700',
+      primary2Color: '#0E376F',
+      primary3Color: '#7B68EE',
+      textColor: '#0E376F',
+      alternateTextColor: '#0E376F'
+    }
+});
+
 
  const App = (props) => {
     return (
       <div>
-         <MuiThemeProvider>
+         <MuiThemeProvider muiTheme={muiTheme}>
              { props.children }
         </MuiThemeProvider>
       </div>
