7546589d1cfe453523ea07a86c6f0880d195c4d4
Bogdan Pshonyak <bpshonyak@live.com>
2016-07-10 14:52:08 +0900

    inital react/redux app setup

--- app/index.js
+++ app/index.js
@@ -1,2 +1,22 @@
+// 3rd party libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+import promise from 'redux-promise';
+
+// local js files
+import reducers from './reducers';
+import routes from './routes';
+
+// stylesheets
 import './style/style.scss';
-console.log("loaded index.js");
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
+  , document.querySelector('#app_root'));
