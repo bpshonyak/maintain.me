7e9d8c118a849598b4c9553731b0f9f54455ac0f
Bogdan Pshonyak <bpshonyak@live.com>
2016-08-25 02:12:50 +0900

    removed unused packages

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import Nav from './navbar';
-
-const muiTheme = getMuiTheme({
-    palette: {
-      primary1Color: '#FFD700',
-      primary2Color: '#0E376F',
-      primary3Color: '#7B68EE',
-      textColor: '#0E376F',
-      alternateTextColor: '#0E376F'
-    }
-});
 
  const App = (props) => {
     return (
       <div>
-         <MuiThemeProvider muiTheme={muiTheme}>
-             <div>
-                <Nav />
-                { props.children }
-             </div>
-        </MuiThemeProvider>
+          <h1>Hello World</h1>
       </div>
     );
 }
