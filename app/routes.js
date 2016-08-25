298c81311906aa3837a477a6b30a97249a983801
Bogdan Pshonyak <bpshonyak@live.com>
2016-08-25 14:55:41 +0900

    added higher order component to protect routes.

--- app/routes.js
+++ app/routes.js
@@ -3,17 +3,24 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 
+// Auth routes
 import LoginForm from './components/auth/login_form';
 import SignUpForm from './components/auth/signup_form';
 import Logout from './components/auth/logout';
+import RequiredAuth from './components/auth/require_auth';
 
+// Unprotected routes
+import Welcome from './components/welcome';
+
+//Protected routes
 import Dashboard from './components/dashboard';
 
 export default (
   <Route path="/" component={App}>
+    <IndexRoute component={Welcome} />
     <Route path="login" component={LoginForm} />
     <Route path="logout" component={Logout} />
     <Route path="signup" component={SignUpForm} />
-    <Route path="dashboard" component={Dashboard} />
+    <Route path="dashboard" component={RequiredAuth(Dashboard)} />
   </Route>
 )
