e0f61e16ab0fb650a04f55c6f7dda21ecdb1f243
Bogdan Pshonyak <bpshonyak@live.com>
2016-08-25 10:44:21 +0900

    added redux-thunk

--- app/components/login_form.jsx
+++ app/components/login_form.jsx
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class LoginForm extends Component {
 
-  handleSubmit(e) {
-    e.preventDefault();
-    //TODO: Handle submittion
+  handleFormSubmit({email, password}) {
+    console.log(email, password);
   }
 
   render() {
+
+    const { handleSubmit, fields: {email, password} } = this.props;
+
     return (
       <div>
         <div className="row">
-          <form className="col s12" onSubmit={this.handleSubmit}>
+          <form className="col s12" onSubmit={handleSubmit(this.handleFormSubmit).bind(this)}>
             <div className="row">
               <div className="input-field col s12">
-                <input id="password" type="password" className="validate" />
-                <label>Password</label>
+                <input {...email} id="email" type="email" className="validate" />
+                <label>Email</label>
               </div>
             </div>
             <div className="row">
               <div className="input-field col s12">
-                <input id="email" type="email" className="validate" />
-                <label>Email</label>
+                <input {...password} id="password" type="password" className="validate" />
+                <label>Password</label>
               </div>
             </div>
             <div>
@@ -36,4 +39,7 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm;
+export default reduxForm({
+  form: 'login',
+  fields: ['email', 'password']
+})(LoginForm);
